7d716f7a2e37466f062d5cd379b92adc24a32845
Sagnik Mukherjee <mukherjee.sagnik17@gmail.com>
2024-09-25 23:55:53 +0530

    permissions added

--- app/src/main/java/app/sagnikmukherjee/androidcamerax/MainActivity.kt
+++ app/src/main/java/app/sagnikmukherjee/androidcamerax/MainActivity.kt
@@ -20,28 +20,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             AndroidCameraXTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Text(text = "Camera")
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidCameraXTheme {
-        Greeting("Android")
     }
 }
