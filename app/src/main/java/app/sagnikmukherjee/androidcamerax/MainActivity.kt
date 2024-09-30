b0dbd22181ee9dd7dde97a8ed70a1fd8f24c748e
itssagnikmukherjee <mukherjee.sagnik17@gmail.com>
2024-09-30 23:45:54 +0530

    camera screen added

--- app/src/main/java/app/sagnikmukherjee/androidcamerax/MainActivity.kt
+++ app/src/main/java/app/sagnikmukherjee/androidcamerax/MainActivity.kt
@@ -2,13 +2,19 @@ package app.sagnikmukherjee.androidcamerax
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import app.sagnikmukherjee.androidcamerax.ui.theme.AndroidCameraXTheme
@@ -20,10 +26,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             AndroidCameraXTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Permission()
                 }
             }
         }
@@ -31,17 +34,26 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun Permission() {
+    val permission = listOf(
+        android.Manifest.permission.CAMERA,
     )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidCameraXTheme {
-        Greeting("Android")
+    val isGranted = remember { mutableStateOf(false) }
+    val launch = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestMultiplePermissions(),
+        onResult = {
+            isGranted.value = it.all { it.value }
+        }
+    )
+    if (isGranted.value){
+//        TODO
+    }else{
+        Column {
+            Button(onClick = {
+                launch.launch(permission.toTypedArray())
+            }) {
+                Text("Request Permission")
+            }
+        }
     }
 }
---
+++ app/src/main/java/app/sagnikmukherjee/androidcamerax/camera.kt
@@ -0,0 +1,11 @@
+package app.sagnikmukherjee.androidcamerax
+
+import androidx.camera.core.CameraSelector
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun CameraScreen(modifier: Modifier = Modifier) {
+    val lens = CameraSelector.LENS_FACING_BACK
+
+}
